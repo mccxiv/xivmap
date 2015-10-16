fa7673dba12b82687f38ea2abf945d2825cc6bb1
Andrea Stella <adr.stella@gmail.com>
2015-10-15 19:06:48 -0600

    Prepare packaging tasks

--- gulpfile.js
+++ gulpfile.js
@@ -19,3 +19,21 @@ gulp.task('clean-up-gh-pages', function() {
 gulp.task('gh-pages', function(cb) {
 	runSequence('clean-up-gh-pages', 'prep-files-gh-pages', 'deploy-gh-pages', 'clean-up-gh-pages', cb);
 });
+
+gulp.task('package', function(cb) {
+	runSequence('clean-packaged', 'copy-demo', 'copy-xivmap', cb);
+});
+
+gulp.task('clean-packaged', function() {
+	return del('packaged/');
+});
+
+gulp.task('copy-demo', function() {
+	return gulp.src(['demo/**', '!demo/xivmap-demo.gif', '!demo/xivmap-demo.mp4'])
+		.pipe(gulp.dest('packaged/demo/'));
+});
+
+gulp.task('copy-xivmap', function() {
+	return gulp.src(['xivmap.js', 'xivmap.css', 'xivmap-docked.css'])
+		.pipe(gulp.dest('packaged/xivmap/'));
+});
